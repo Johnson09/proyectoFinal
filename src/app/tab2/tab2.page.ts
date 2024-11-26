bcf021134db20202eec2df76e3bea64b301dd537
Diego Velasquez <dfvelasquez43@gmail.com>
2024-11-25 19:46:23 -0500

    update tab2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { LoadingController } from '@ionic/angular';
+import { LoadingController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,8 +7,13 @@ import { LoadingController } from '@ionic/angular';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  coordinacion: string = '';
+  descripcion: string = '';
+  areas: any[] = [];
 
-  constructor(private loadingController: LoadingController) { }
+  constructor(private loadingController: LoadingController, private alertController: AlertController) {
+    this.loadAreas();
+  }
 
   async showLoading() {
     const loading = await this.loadingController.create({
@@ -16,6 +21,54 @@ export class Tab2Page {
       duration: 2000
     });
     await loading.present();
+  }
+
+  async showAlert() {
+    const alert = await this.alertController.create({
+      header: 'Error',
+      message: 'Por favor, complete todos los campos.',
+      buttons: ['OK']
+    });
+    await alert.present();
+  }
+
+  saveArea() {
+    if (this.coordinacion.trim() === '' || this.descripcion.trim() === '') {
+      this.showAlert();
+      return;
+    }
+
+    const area = {
+      coordinacion: this.coordinacion,
+      descripcion: this.descripcion
+    };
+    let areas = JSON.parse(localStorage.getItem('areas') || '[]');
+    areas.push(area);
+    localStorage.setItem('areas', JSON.stringify(areas));
+    this.showLoading();
+    this.loadAreas();
+    this.clearForm();
+  }
+
+  loadAreas() {
+    this.areas = JSON.parse(localStorage.getItem('areas') || '[]');
+  }
+
+  clearForm() {
+    this.coordinacion = '';
+    this.descripcion = '';
+  }
+
+  clearAreas() {
+    localStorage.removeItem('areas');
+    this.loadAreas();
+  }
+
+  deleteArea(index: number) {
+    let areas = JSON.parse(localStorage.getItem('areas') || '[]');
+    areas.splice(index, 1);
+    localStorage.setItem('areas', JSON.stringify(areas));
+    this.loadAreas();
   }
 
 }
